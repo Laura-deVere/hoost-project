e0fc7c09e94de8e560a15ab874f995fcd15f6a26
laura devere <lauradevh@gmail.com>
2016-07-01 12:53:07 -0400

    testing heroku

---
+++ public/assets/components/search_bar-aab963db7279061faa8b45d3686d2abb597a117148e933a657a7ef2cd74d94a1.jsx
@@ -0,0 +1,18 @@
+class SearchBar extends React.Component {
+	citySearch() {
+		let query = this.refs.searchInput.value;
+		this.props.onSearch(query);
+	}
+
+	render() {
+		return (
+			<section className="hero">
+				<div className="search-bar">
+					<h2>Adventure is NOT worthwhile</h2>
+						<input ref="searchInput" onChange={this.citySearch.bind(this)} 
+							value={this.props.query} placeholder="Search..."/>
+				</div>	
+			</section>
+		)
+	}
+};
